460a96f9cd29bb757a1a37b66fb80a0efb173bac
saadi-rahman <sadi6280@gmail.com>
2022-09-02 10:34:58 +0600

    toggle no phone found message based on search result

--- js/app.js
+++ js/app.js
@@ -8,6 +8,17 @@ const loadPhones = async(searchText) =>{
 const displayPhones = phones =>{
     const phonesContainer = document.getElementById("phones-container");
     phonesContainer.textContent = "";
+    // display 20 phones only
+    phones = phones.slice(0, 20);
+    // display no phone found
+    const noPhone = document.getElementById("alert-text");
+    if(phones.length === 0){
+        noPhone.classList.remove("d-none");
+    }
+    else {
+        noPhone.classList.add("d-none");
+    }
+    // display all phones
     phones.forEach(phone =>{
         const phoneDiv = document.createElement("div");
         phoneDiv.classList.add("col");
@@ -16,7 +27,8 @@ const displayPhones = phones =>{
         <img src="${phone.image}" class="card-img-top px-3 pt-3" alt="...">
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text">Brand: ${phone.brand}</p>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
         </div>
@@ -31,4 +43,4 @@ document.getElementById("btn-search").addEventListener("click", function(){
     loadPhones(searchText);
 })
 
-loadPhones();
+loadPhones("iPhone");
